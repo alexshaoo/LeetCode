b8979dd4332418a26d2f667927290156d18de6b5
Alex Shao <alexshao90@gmail.com>
2022-10-13 21:02:18 -0400

    Time: 49 ms (93.82%), Space: 13.9 MB (36.90%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -3,5 +3,4 @@ class Solution:
     def backtrack(queens=[], d1=[], d2=[]):
       l = len(queens)
       return (l == n) + sum([backtrack(queens+[j], d1+[j-l], d2+[j+l]) for j in range(n) if j not in queens and j-l not in d1 and j+l not in d2])
-    
     return backtrack()
