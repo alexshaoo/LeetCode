84ef8481c1e9e6aa049e26b983f02bf5e9334ac8
Alex Shao <alexshao90@gmail.com>
2022-10-13 20:50:43 -0400

    Time: 52 ms (91.66%), Space: 14.1 MB (9.80%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+  def totalNQueens(self, n: int) -> int:
+    col = set()
+    pos = set()
+    neg = set()
+    
+    ans = []
+    board = [['.']*n for _ in range(n)]
+    
+    def backtrack(r):
+      if r == n:
+        ans.append(["".join(row) for row in board])
+        return
+      
+      for c in range(n):
+        if c not in col and r+c not in pos and r-c not in neg:
+          
+          board[r][c] = "Q"
+          col.add(c)
+          pos.add(r+c)
+          neg.add(r-c)
+          
+          backtrack(r+1)
+          
+          board[r][c] = '.'
+          col.remove(c)
+          pos.remove(r+c)
+          neg.remove(r-c)
+          
+    backtrack(0)
+    
+    return len(ans)
