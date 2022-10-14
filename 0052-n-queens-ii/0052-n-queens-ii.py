ce59e715417b9e55e40913e57bef285ab2e92f8a
Alex Shao <alexshao90@gmail.com>
2022-10-13 21:01:54 -0400

    Time: 64 ms (82.77%), Space: 13.9 MB (79.98%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -2,9 +2,6 @@ class Solution:
   def totalNQueens(self, n: int) -> int:
     def backtrack(queens=[], d1=[], d2=[]):
       l = len(queens)
-      c = 0
-      if l == n:
-        c += 1
-      return c + sum([backtrack(queens+[j], d1+[j-l], d2+[j+l]) for j in range(n) if j not in queens and j-l not in d1 and j+l not in d2])
+      return (l == n) + sum([backtrack(queens+[j], d1+[j-l], d2+[j+l]) for j in range(n) if j not in queens and j-l not in d1 and j+l not in d2])
     
     return backtrack()
