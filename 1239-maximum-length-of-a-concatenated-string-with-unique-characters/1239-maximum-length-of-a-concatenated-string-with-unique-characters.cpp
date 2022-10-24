6fc85bfa406a7fbc28e98a9af9642e1ec1896372
Alex Shao <alexshao90@gmail.com>
2022-10-24 02:19:52 -0400

    Time: 5 ms (93.90%), Space: 9.5 MB (77.45%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -9,14 +9,15 @@ public:
     for (const auto& s : arr) {
       bitset<26> next;
       for (const auto& c : s) next.set(c - 'a');
-      if (next.count() < s.size()) continue;
+      int n = next.count();
+      if (n < s.size()) continue;
     
       // bitop
       for (int i = 0; i < dp.size(); ++i) {
         bitset curr = dp[i];
         if ((curr & next).any()) continue;
         dp.push_back(curr | next);
-        res = max(res, (int)(curr.count() + next.count()));
+        res = max(res, (int)(curr.count()) + n);
       }
     }
     
