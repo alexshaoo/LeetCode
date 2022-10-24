7018e3f3e35d9ca181e813fc798211f36629d5dc
Alex Shao <alexshao90@gmail.com>
2022-10-24 02:18:56 -0400

    Time: 3 ms (98.05%), Space: 9.6 MB (77.45%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
   int maxLength(vector<string>& arr) {
+    // vector of one empty bitset initially so the first element in arr is automatically added
     vector<bitset<26>> dp = {bitset<26>()};
     int res = 0;
     
     // input dupes
     for (const auto& s : arr) {
-      bitset<26> a;
-      for (const auto& c : s) a.set(c - 'a');
-      if (a.count() < s.size()) continue;
+      bitset<26> next;
+      for (const auto& c : s) next.set(c - 'a');
+      if (next.count() < s.size()) continue;
     
       // bitop
       for (int i = 0; i < dp.size(); ++i) {
-        if ((dp[i] & a).any()) continue;
-        dp.push_back(dp[i] | a);
-        res = max(res, (int)dp[i].count() + (int)a.count());
+        bitset curr = dp[i];
+        if ((curr & next).any()) continue;
+        dp.push_back(curr | next);
+        res = max(res, (int)(curr.count() + next.count()));
       }
     }
     
