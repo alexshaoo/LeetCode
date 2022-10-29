394aff42ee3c0c887f40393aaa98a95640d4ba9a
Alex Shao <alexshao90@gmail.com>
2022-10-29 14:44:42 -0400

    Time: 61 ms (29.09%), Space: 13.9 MB (59.17%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,3 +1,11 @@
 class Solution:
   def isStrictlyPalindromic(self, n: int) -> bool:
-    return str(bin(n)) == str(bin(n))[::-1]
+    for base in range(2, n-1):
+      st = ""
+      k = n
+      while k > 0:
+        st += str(k%base)
+        k //= base
+      if st != st[::-1]:
+        return False
+    return True
