3fad551f8e72658aa74649c09ee29275c79c84d9
Alex Shao <alexshao90@gmail.com>
2022-11-22 16:02:08 -0500

    Time: 2074 ms (57.29%), Space: 13.9 MB (88.58%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -2,5 +2,6 @@ class Solution:
   def numSquares(self, n: int) -> int:
     dp = [0] * (n+1)
     for i in range(1,n+1):
-      dp[i] = min([dp[i-j*j] for j in range(1,int(i**.5)+1)])+1
+      sqrt = int(i**0.5)
+      dp[i] = 1+min([dp[i-j*j] for j in range(1,sqrt+1)])
     return dp[n]
