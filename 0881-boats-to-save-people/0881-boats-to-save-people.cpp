f679d3247418d96d095ae173a4ac32e25b91a670
Alex Shao <alexshao90@gmail.com>
2022-11-30 21:42:44 -0500

    Time: 88 ms (94.75%), Space: 42 MB (54.32%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  int numRescueBoats(vector<int>& people, int limit) {
+    int l = 0;
+    int r = people.size()-1;
+    int b = 0;
+    sort(people.begin(), people.end());
+    while (l <= r) {
+      if (people[l] + people[r] <= limit) {
+        ++l;
+        --r;
+      } else {
+        --r;
+      }
+      ++b;
+    }
+    return b;
+  }
+};
