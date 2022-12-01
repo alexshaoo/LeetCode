ca0ee198d1118a6bbb72e98bf9400d19160d28ac
Alex Shao <alexshao90@gmail.com>
2022-11-30 21:43:41 -0500

    Time: 88 ms (94.75%), Space: 42 MB (54.32%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,13 +1,19 @@
-class Solution:
-  def numRescueBoats(self, people: List[int], limit: int) -> int:
-    l = len(people)
-    people.sort()
-    l, r, b = 0, l-1, 0
-    while l <= r:
-      if people[l] + people[r] <= limit:
-        l += 1
-        r -= 1
-      else:
-        r -= 1
-      b += 1
-    return b
+class Solution {
+public:
+  int numRescueBoats(vector<int>& people, int limit) {
+    int l = 0;
+    int r = people.size()-1;
+    int b = 0;
+    sort(people.begin(), people.end());
+    while (l <= r) {
+      if (people[l] + people[r] <= limit) {
+        ++l;
+        --r;
+      } else {
+        --r;
+      }
+      ++b;
+    }
+    return b;
+  }
+};
