0b45da0816dfc7f3751dd853b63141d9cb24a0dd
Alex Shao <alexshao90@gmail.com>
2022-12-04 00:43:35 -0500

    Time: 1034 ms (92.79%), Space: 24.9 MB (80.12%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,18 +1,5 @@
 class Solution:
   def minimumAverageDifference(self, nums: List[int]) -> int:
-    l = len(nums)
-    ps = [0]
-    for num in nums:
-      ps.append(num + ps[-1])
-    mi, m = -1, inf
-    for i in range(1, l):
-      # print(ps[i], ps[l]-ps[i], i, l-i)
-      d = abs(ps[i]//i-(ps[l]-ps[i])//(l-i))
-      if d < m:
-        mi = i-1
-        m = d
-      # print(d)
-      i += 1
-    if ps[-1]//l < m:
-      return l-1
-    return mi
+    tot = sum(nums)
+    n = len(nums)
+    return min(((abs(s//(i+1)-((tot-s)//(n-i-1) if i != n-1 else 0))), i) for i,s in enumerate(accumulate(nums)))[1]
