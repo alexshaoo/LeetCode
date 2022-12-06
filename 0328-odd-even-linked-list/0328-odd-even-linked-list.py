94206147f228d2b7ef9fa2aa723c9e141267121b
Alex Shao <alexshao90@gmail.com>
2022-12-06 02:21:52 -0500

    Time: 125 ms (5.41%), Space: 16.6 MB (31.02%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -3,13 +3,5 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-  def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    if not head or not head.next: 
-      return head
-    r, odd, p, head = head, head, head.next, head.next.next
-    while head:
-      odd.next, head.next, p.next = head, odd.next, head.next
-      p, odd, head = p.next, head, p.next and p.next.next
-    return r
+class Solution: oddEvenList = lambda _,r: (h := [None]*2, t := [None]*2, all((setitem(h, i, x), setitem(t, i, x)) if h[i] == None else (setattr(t[i], 'next', x), setitem(t, i, x)) for i, x in zip(cycle(range(2)),takewhile(lambda x:x, accumulate(repeat(r), lambda z,_:z.next)))), setattr(t[0], 'next', h[1]) if t[0] else None, setattr(t[1], 'next', None) if t[1] else None, r)[-1]
     
