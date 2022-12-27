c0fa4a9997827ca34409cbdf285dc685ea056598
Alex Shao <alexshao90@gmail.com>
2022-12-26 22:52:26 -0800

    Time: 943 ms (93.42%), Space: 22.4 MB (32.89%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,12 +1,11 @@
 class Solution:
   def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-    rocks = [c-r for c, r in zip(capacity, rocks)]
-    rocks.sort()
+    rocks = sorted([c-r for c, r in zip(capacity, rocks)])
     n = len(rocks)
     i = 0
-    print(rocks)
+    # print(rocks)
     while i < n and additionalRocks >= rocks[i]:
-      print(additionalRocks, i)
+      # print(additionalRocks, i)
       additionalRocks -= rocks[i]
       i += 1
     return i
