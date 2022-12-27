3d53f4d0ba319228f59883410b1d457e496d3599
Alex Shao <alexshao90@gmail.com>
2022-12-26 23:00:37 -0800

    Time: 878 ms (99.67%), Space: 23.1 MB (16.12%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -1,7 +1,3 @@
 class Solution:
   def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
-    rocks = sorted([c-r for c, r in zip(capacity, rocks)])
-    n = len(rocks)
-    i = 0
-    # print(rocks)
-    return bisect_right(list(accumulate(rocks)), additionalRocks)
+    return bisect_right(list(accumulate(sorted([c-r for c, r in zip(capacity, rocks)]))), additionalRocks)
