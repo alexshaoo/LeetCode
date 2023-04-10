0f065ed57767ac6308628e6352684903a2941c53
Alex Shao <alexshao90@gmail.com>
2023-04-09 23:53:04 -0700

    Time: 35 ms (47.77%), Space: 13.8 MB (96.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,3 +1,13 @@
 class Solution:
   def isValid(self, s: str) -> bool:
-    return s == '' if s == (s:=s.replace('()', '').replace('{}', '').replace('[]', '')) else self.isValid(s)
+    opening = ["(", "{", "["]
+    closing = [")", "}", "]"]
+    pairs = dict(zip(closing, opening))
+    stack = []
+    for paren in s: 
+      if paren in opening: 
+        stack.append(paren)
+      if paren in closing: 
+        if not stack or stack.pop() != pairs[paren]: 
+          return False
+    return len(stack) == 0
