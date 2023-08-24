96ceb46d38ee594c5f7264c096293ba3a8aa9052
Alex Shao <alexshao90@gmail.com>
2023-08-24 18:39:18 -0400

    Time: 37 ms (86.24%) | Memory: 16.5 MB (31.00%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -2,12 +2,12 @@ class Solution:
   def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
     res, cur, n = [], [], 0
     for w in words:
-        if n + len(w) + len(cur) > maxWidth:
-            for i in range(maxWidth - n):
-                cur[i%(len(cur)-1 or 1)] += ' '
-            res.append(''.join(cur))
-            cur, n = [], 0
-        cur += [w]
-        n += len(w)
+      if n + len(w) + len(cur) > maxWidth:
+        for i in range(maxWidth - n):
+          cur[i%(len(cur)-1 or 1)] += ' '
+        res.append(''.join(cur))
+        cur, n = [], 0
+      cur += [w]
+      n += len(w)
     return res + [' '.join(cur).ljust(maxWidth)]
     
