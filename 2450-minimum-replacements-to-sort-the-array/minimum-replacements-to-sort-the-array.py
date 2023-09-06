ac7ed1214a4368de96470caaa0fb2eaeb5728e07
Alex Shao <alexshao90@gmail.com>
2023-09-05 23:37:12 -0700

    Time: 494 ms (34.12%) | Memory: 28.2 MB (70.02%) - LeetSync

--- 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.py
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.py
@@ -1,10 +1,11 @@
 class Solution:
   def minimumReplacement(self, nums: List[int]) -> int:
-    ans=0
-    prev=10**9+1
-    for x in nums[::-1]:
-      d=ceil(x/prev)
-      ans+=d-1
-      prev=x//d
-
-    return ans    
+    res = 0
+    n = len(nums)
+    for i in range(n-2, -1, -1):
+      if nums[i] <= nums[i+1]:
+        continue
+      c = (nums[i] + nums[i + 1] - 1) // nums[i + 1]
+      res += c - 1
+      nums[i] = nums[i] // c
+    return res
