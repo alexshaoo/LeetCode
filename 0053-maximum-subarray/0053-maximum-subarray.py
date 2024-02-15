cec461791157f92d9335d5b66ab8f0beb454c63c
Alex Shao <alexshao90@gmail.com>
2024-02-15 10:52:53 -0500

    Time: 539 ms (60.67%), Space: 31.1 MB (57.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,14 +3,8 @@ class Solution:
     n = len(nums)
     if all([num <= 0 for num in nums]):
       return max(nums)
-    l, r = 0, 0
     mx, sm = 0, 0
-    while r < n:
-      sm += nums[r]
-      if sm < 0:
-        sm = 0
-        l = r
-      else:
-        mx = max(mx, sm)
-      r += 1
+    for num in nums:
+      sm = max(0, sm + num)
+      mx = max(mx, sm)
     return mx
