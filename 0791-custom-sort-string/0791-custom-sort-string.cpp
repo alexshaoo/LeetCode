1eb4d9e9425c50a78bafa7d6c34c9ad329e3b625
Alex Shao <alexshao90@gmail.com>
2024-03-12 00:42:28 -0400

    Time: 0 ms (100.00%), Space: 7.3 MB (85.55%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        unordered_map<char, pair<int, bool>> c;
-        for (auto ch : s) {
-            if (c.find(ch) != c.end()) c[ch].first++;
-            else c[ch].first = 1;
+        int fre[26] = {0};
+        bitset<26> ord;
+        for (auto c : order) {
+            ord[c - 'a'] = 1;
         }
-        string ans = "";
-        for (auto ch : order) {
-            if (c.find(ch) != c.end()) {
-                for (int i = 0 ; i < c[ch].first; ++i) ans += ch;
-                c[ch].second = true;
+        string ans;
+        for (auto c : s) {
+            if (ord[c - 'a'] == 0) {
+                ans += c;
+            } else {
+                ++fre[c - 'a'];
             }
         }
-        for (auto ch : s) {
-            if (!c[ch].second) {
-                ans += ch;
-            }
+        for (auto c : order) {
+            ans += string(fre[c - 'a'], c);
         }
         return ans;
     }
