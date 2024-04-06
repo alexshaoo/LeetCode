e484bff032a2372e11849479d8f8f5fc56f406d5
Alex Shao <alexshao90@gmail.com>
2024-04-05 23:18:33 -0400

    Time: 15 ms (92.59%), Space: 12.2 MB (79.62%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,22 +1,28 @@
 class Solution {
-    public:
-        string minRemoveToMakeValid(string s) {
-        int o = 0, c = count(s.begin(), s.end(), ')');
+public:
+    string minRemoveToMakeValid(string s) {
+        int open = 0;
+        int totalClose = count(s.begin(), s.end(), ')');
+
         string ans = "";
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(' && c != 0) {
-                ans += s[i];
-                --c;
-                ++o;
-            } else if (s[i] == ')' && o != 0) {
-                ans += s[i];
-                --o;
-            } else if (s[i] == ')') {
-                --c;
-            } else if (s[i] != ')' && s[i] != '(') {
-                ans += s[i];
+        for (char ch : s) {
+            if (ch == '(') {
+                if (totalClose > 0) {
+                    ans += ch;
+                    open++;
+                    totalClose--;
+                }
+            } else if (ch == ')') {
+                if (open > 0) {
+                    ans += ch;
+                    open--;
+                } else {
+                    totalClose--;
+                }
+            } else {
+                ans += ch;
             }
         }
-        return ans;        
+        return ans;
     }
 };
