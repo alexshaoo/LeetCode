de638ebd2e2f98aded09ab551194bda0bb2f8347
Alex Shao <alexshao90@gmail.com>
2024-07-14 21:31:40 -0400

    [2062] [Time Beats: 100.00%] [Memory Beats: 51.55%] - LeetPush

--- 2062-Count-Vowel-Substrings-of-a-String.cpp
+++ 2062-Count-Vowel-Substrings-of-a-String.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int countVowelSubstrings(string word) {
+    int f(string& word, int k) {
         auto v = [](char c) -> bool {
-            return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+            return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
         };
         int n = word.length();
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            unordered_set<char> s;
-            for (int j = i; j < n; ++j) {
-                if (!v(word[j])) break;
-                s.insert(word[j]);
-                if (s.size() == 5) ++ans;
+        unordered_map<char, int> mp;
+        int i = 0;
+        for (int j = 0; j < n; ++j) {
+            if (!v(word[j])) {
+                i = j + 1;
+                mp.clear();
+                continue;
             }
+            mp[word[j]]++;
+            while (mp.size() > k) {
+                mp[word[i]]--;
+                if (mp[word[i]] == 0) mp.erase(word[i]);
+                ++i;
+            }
+            ans += j-i+1;
         }
         return ans;
+    }
+    int countVowelSubstrings(string word) {
+        return f(word, 5) - f(word, 4);
     }
 };
