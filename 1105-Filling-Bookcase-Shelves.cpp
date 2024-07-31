15bd6454367d35dbfa9d8c5db4ff78718e2c6dfa
Alex Shao <alexshao90@gmail.com>
2024-07-31 02:07:10 -0400

    [1105] [Time Beats: 88.81%] [Memory Beats: 49.85%] - LeetPush

--- 1105-Filling-Bookcase-Shelves.cpp
+++ 1105-Filling-Bookcase-Shelves.cpp
@@ -6,15 +6,14 @@ public:
         dp[0] = 0;
         int n = books.size();
         for (int i = 1; i <= n; ++i) {
-            int k = 0, w = 0, h = 0;
-            while (k < i && w <= shelfWidth - books[i-k-1][0]) {
-                h = max(h, books[i-k-1][1]);
-                // cout << i-k-1 << ' ' << dp[i-k-1] << ' ' << h << '\n';
-                dp[i] = min(dp[i], dp[i-k-1] + h);
-                w += books[i-k-1][0];
+            int k = 1, w = 0, h = 0;
+            while (k <= i) {
+                h = max(h, books[i-k][1]);
+                w += books[i-k][0];
+                if (w > shelfWidth) break;
+                dp[i] = min(dp[i], dp[i-k] + h);
                 ++k;
             }
-            // cout << "i is " << i << ", dp[i]: " << dp[i] << '\n';
         }
         return dp[n];
     }
