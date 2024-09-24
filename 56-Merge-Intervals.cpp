09a7479057311e80f1f3d0dcae5be845ce5f4eee
Alex Shao <alexshao90@gmail.com>
2024-09-24 17:07:33 +0200

    [56] [Time Beats: 40.23%] [Memory Beats: 37.60%] - LeetPush

--- 56-Merge-Intervals.cpp
+++ 56-Merge-Intervals.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
-        for (int i = 1; i < n; ++i) {
-            if (end >= intervals[i][0]) {
-                end = max(end, intervals[i][1]);
+        for (const auto& interval : intervals) {
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
             } else {
-                ans.push_back({start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+                ans.back()[1] = max(ans.back()[1], interval[1]);
             }
         }
-        ans.push_back({start, end});
         return ans;
     }
 };
