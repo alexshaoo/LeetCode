78074e1f7fb1a35d32a4b3fc08744627dfa95470
Alex Shao <alexshao90@gmail.com>
2025-02-16 23:32:37 -0800

    [1079] [Time Beats: 46.19%] [Memory Beats: 48.73%] - LeetPush

---
+++ 1079-Letter-Tile-Possibilities.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTilePossibilities(string tiles) {
+        int n = tiles.size();
+        sort(tiles.begin(), tiles.end());
+        unordered_set<string> s;
+        do {
+            for (int i = 0; i <= n; ++i) s.insert(tiles.substr(0, i));
+        } while (next_permutation(tiles.begin(), tiles.end()));
+        return s.size()-1;
+    }
+};
