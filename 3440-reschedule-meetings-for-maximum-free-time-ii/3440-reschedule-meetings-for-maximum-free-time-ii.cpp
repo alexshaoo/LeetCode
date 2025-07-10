d2a311657cb2925b69695b9585091972a9af00a2
Alex Shao <alexshao90@gmail.com>
2025-07-09 20:42:12 -0700

    Time: 8 ms (85.66%), Space: 145.4 MB (68.53%) - LeetHub

--- 3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
         int n = startTime.size();
-        vector<int> emp;
-        emp.push_back(startTime[0]);
+        vector<int> gap;
+        gap.push_back(startTime[0]);
         for (int i = 0; i < n-1; ++i) {
-            emp.push_back(startTime[i+1] - endTime[i]);
+            gap.push_back(startTime[i+1] - endTime[i]);
         }
-        emp.push_back(eventTime-endTime[n-1]);
-        multiset<int, greater<int>> gaps;
-        for (int e : emp) gaps.insert(e);
+        gap.push_back(eventTime-endTime[n-1]);
+        vector<bool> b(n, false);
+        int mx = gap[0];
+        for (int i = 1; i < n; ++i) {
+            if (mx >= endTime[i]-startTime[i]) b[i] = true;
+            mx = max(mx, gap[i]);
+        }
+        mx = gap[n];
+        for (int i = n-2; i >= 0; --i) {
+            if (mx >= endTime[i]-startTime[i]) b[i] = true;
+            mx = max(mx, gap[i+1]);
+        }
         int ans = 0;
         for (int i = 0; i < n; ++i) {
             int curGap = endTime[i]-startTime[i];
-            int lgap = emp[i], rgap = emp[i+1];
-            gaps.erase(gaps.find(lgap));
-            gaps.erase(gaps.find(rgap));
-            if (*gaps.begin() >= curGap) { // can be moved to other gap
+            int lgap = gap[i], rgap = gap[i+1];
+            if (b[i]) { // can be moved to other gap
                 ans = max(ans, lgap+curGap+rgap);
             } else { // shifted within current gap
                 ans = max(ans, lgap+rgap);
             }
-            gaps.insert(lgap);
-            gaps.insert(rgap);
         }
         return ans;
     }
