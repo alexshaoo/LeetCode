a8c1aa66079b0b7d83368a88ee0cee5af84f389c
Alex Shao <alexshao90@gmail.com>
2025-08-08 21:00:06 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,8 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         if (n <= 0) return false;
         while (n > 1) {
-            if (n%2==1) return false;
-            n /= 2;
+            if (n&1) return false;
+            n >>= 2;
         }
         return true;
     }
