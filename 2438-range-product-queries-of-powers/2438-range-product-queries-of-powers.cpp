45cfdb5e96f1f785ed4bb663a70e541543d044c4
Alex Shao <alexshao90@gmail.com>
2025-08-10 22:05:03 -0700

    Time: 87 ms (26.6%), Space: 147.7 MB (58.51%) - LeetHub

---
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -0,0 +1,36 @@
+#define ll long long
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    ll modpow(ll base, ll exp) {
+        ll ans = 1;
+        while (exp > 0) {
+            if (exp & 1) {
+                ans = (ans * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return ans;
+    }
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> powers;
+        int currPow = 1;
+        while (n > 0) {
+            if (n & 1) powers.push_back(currPow);
+            currPow *= 2;
+            n >>= 1;
+        }
+        vector<ll> mulP = {1};
+        for (int i = 0; i < powers.size(); ++i) {
+            mulP.push_back((mulP[i] * (ll)powers[i]) % MOD);
+        }
+        vector<int> ans;
+        for (int i = 0; i < queries.size(); ++i) {
+            ll inv = modpow(mulP[queries[i][0]], MOD-2);
+            ll end = mulP[queries[i][1]+1];
+            ans.push_back((end*inv) % MOD);
+        }
+        return ans;
+    }
+};
