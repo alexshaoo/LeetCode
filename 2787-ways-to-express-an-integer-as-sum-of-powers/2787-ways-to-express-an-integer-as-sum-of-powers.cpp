38b61b724aa708cce5df2caf8947d55039b2a024
Alex Shao <alexshao90@gmail.com>
2025-08-11 19:30:13 -0700

    Time: 10 ms (98.04%), Space: 12.7 MB (76.25%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     static int constexpr MOD = 1e9+7;
     int numberOfWays(int n, int x) {
-        // dp[i][j] means number of ways to use first i integers to form sum j
-        vector<vector<int>> dp;
-        vector<int> init(n+1, 0);
-        dp.push_back(init);
-        dp[0][0] = 1;
-        vector<long long> pows(n+1);
-        for (int p = 0; p <= n; ++p) {
-            long long val = 1;
-            for (int _ = 0; _ < x; ++_) val *= p;
-            pows[p] = val;
+        // dp[i] is the number of ways to make i uniquely
+        vector<int> pows, dp(n+1, 0);
+        dp[0] = 1;
+        for (int a = 1; ; ++a) {
+            long long p = 1;
+            for (int k = 0; k < x; ++k) {
+                p *= a;
+                if (p > n) break;
+            }
+            if (p > n) break;
+            pows.push_back((int)p);
         }
-        for (int i = 1; i <= n; ++i) {
-            long long p = pows[i];
-            dp.push_back(dp.back());
-            for (int j = n; j >= 0; --j) {
-                if (j-p < 0) break;
-                dp[i][j] = (dp[i][j] + dp[i-1][j-p]) % MOD;
+        for (int p : pows) {
+            for (int sm = n; sm >= p; --sm) {
+                int val = dp[sm-p];
+                if (val > 0) {
+                    dp[sm] = (dp[sm] + val) % MOD;
+                }
             }
         }
-        return dp[n][n];
+        return dp[n];
     }
 };
