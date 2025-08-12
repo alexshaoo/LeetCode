9b85d66952ecd98cf109ebe5acc744d4042906c2
Alex Shao <alexshao90@gmail.com>
2025-08-11 19:14:30 -0700

    Time: 113 ms (65.58%), Space: 290 MB (7.84%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -7,8 +7,14 @@ public:
         vector<int> init(n+1, 0);
         dp.push_back(init);
         dp[0][0] = 1;
+        vector<long long> pows(n+1);
+        for (int p = 0; p <= n; ++p) {
+            long long val = 1;
+            for (int _ = 0; _ < x; ++_) val *= p;
+            pows[p] = val;
+        }
         for (int i = 1; i <= n; ++i) {
-            long long p = pow(i, x);
+            long long p = pows[i];
             dp.push_back(dp.back());
             for (int j = n; j >= 0; --j) {
                 if (j-p < 0) break;
