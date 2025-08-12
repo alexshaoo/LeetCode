90c580b78d3fff1686102b9b8d160c5f2f80bf13
Alex Shao <alexshao90@gmail.com>
2025-08-11 19:10:13 -0700

    Time: 105 ms (68.63%), Space: 287.6 MB (7.84%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -3,22 +3,18 @@ public:
     static int constexpr MOD = 1e9+7;
     int numberOfWays(int n, int x) {
         // dp[i][j] means number of ways to use first i integers to form sum j
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        vector<vector<int>> dp;
+        vector<int> init(n+1, 0);
+        dp.push_back(init);
         dp[0][0] = 1;
         for (int i = 1; i <= n; ++i) {
             long long p = pow(i, x);
-            for (int j = n; j >= 0; --j) {
-                dp[i][j] = (dp[i][j] + dp[i-1][j]) % MOD;
-            }
+            dp.push_back(dp.back());
             for (int j = n; j >= 0; --j) {
                 if (j-p < 0) break;
                 dp[i][j] = (dp[i][j] + dp[i-1][j-p]) % MOD;
             }
         }
-        // for (auto a : dp) {
-        //     for (auto b : a) cout << b << ' ';
-        //     cout << '\n';
-        // }
         return dp[n][n];
     }
 };
