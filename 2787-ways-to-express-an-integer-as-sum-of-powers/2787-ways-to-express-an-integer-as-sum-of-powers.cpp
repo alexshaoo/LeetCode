52e4240e0407871a92ca0eaceb8b0eeb9483635e
Alex Shao <alexshao90@gmail.com>
2025-08-11 18:37:16 -0700

    Time: 2449 ms (5.01%), Space: 274.5 MB (28.1%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     static int constexpr MOD = 1e9+7;
-    int f(vector<vector<int>>& dp, int remainingSum, int num, int x) {
-        if (remainingSum < 0 || remainingSum < pow(num, x)) return 0;
-        if (remainingSum == pow(num, x)) return 1;
+    int f(vector<vector<int>>& dp, int remainingSum, int num, int x, long long p) {
+        if (remainingSum < 0 || (long long)remainingSum < p) return 0;
+        if (remainingSum == p) return 1;
         if (remainingSum >= 0 && remainingSum < dp.size() && num >= 0 && num < dp.size()) {
             if (dp[remainingSum][num] != -1) return dp[remainingSum][num];
         }
         // cout << remainingSum << ' ' << num << ' ' << pow(num, x) << '\n';
-        int newRemainingSum = remainingSum - pow(num, x);
+        long long newRemainingSum = (long long)remainingSum - p;
         dp[remainingSum][num] = 0;
         for (int i = num-1; i > 0; --i) {
-            dp[remainingSum][num] = (dp[remainingSum][num] + f(dp, newRemainingSum, i, x)) % MOD;
+            dp[remainingSum][num] = (dp[remainingSum][num] + f(dp, (int)newRemainingSum, i, x, (long long)pow(i, x))) % MOD;
         }
         return dp[remainingSum][num];
     }
@@ -22,7 +22,7 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         int ans = 0;
         for (int i = 1; i <= n; ++i) {
-            ans = (ans + f(dp, n, i, x)) % MOD;
+            ans = (ans + f(dp, n, i, x, (long long)pow(i, x))) % MOD;
         }
         return ans;
     }
