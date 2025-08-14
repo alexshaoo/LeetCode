5ba55222a8e0550818872973beda8652ffc03107
Alex Shao <alexshao90@gmail.com>
2025-08-13 21:24:50 -0700

    Time: 0 ms (100%), Space: 9 MB (77.37%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -5,6 +5,10 @@ public:
         int n = num.length();
         int i = 0;
         while (i <= n-3) {
+            if (ans != "" && num[i] <= ans[0]) {
+                ++i;
+                continue;
+            }
             if (num[i] == num[i+1] && num[i] == num[i+2]) {
                 if (ans == "" || num[i] > ans[0]) {
                     ans = string(3, num[i]);
