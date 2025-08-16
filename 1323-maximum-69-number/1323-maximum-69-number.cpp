a3a27f8fc1f00d14c534172b68a56e3fbc3b73a2
Alex Shao <alexshao90@gmail.com>
2025-08-15 19:04:14 -0700

    Time: 0 ms (100%), Space: 7.8 MB (94.22%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-  int maximum69Number (int num) {
-    string a = to_string(num);
-    for (int i = 0; i< a.length(); ++i) {
-      if (a[i] == '6') {
-        a[i] = '9';
-        break;
-      }
+    int maximum69Number(int num) {
+        // string ans = string(num);
+        // for (char& digit : ans) {
+        //     if (digit == '6') {
+        //         digit = '9';
+        //         return stoi(ans);
+        //     }
+        // }
+        // return num-3;
+        int ans = num;
+        int pow = 1, highestPow = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            num /= 10;
+            if (digit == 6) {
+                highestPow = pow;
+            }
+            pow *= 10;
+        }
+        return ans + (3 * highestPow);
     }
-    return stoi(a);
-  }
 };
