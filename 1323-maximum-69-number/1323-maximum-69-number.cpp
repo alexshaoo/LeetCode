96655d8332415b4e1cfde4c64e3ca98cbcb3522f
Alex Shao <alexshao90@gmail.com>
2025-08-15 19:16:30 -0700

    Time: 0 ms (100%), Space: 8.2 MB (12.18%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int maximum69Number(int num) {
-        int p = -1, di = -1;
+        int p = -1, di = 0;
         int n = num;
-        while (n > 0) {
-            int digit = n % 10;
-            n /= 10;
-            ++di;
-            if (digit == 6) p = di;
+        for (int n = num; n > 0; ++di, n/= 10) {
+            if (n%10 == 6) p = di;
         }
         if (p == -1) return num;
         return num + 3 * pow(10, p);
