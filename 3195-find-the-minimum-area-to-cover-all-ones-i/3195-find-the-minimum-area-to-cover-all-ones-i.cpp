847c80fdc5adf85369966fffd4b33a2fdd67e179
Alex Shao <alexshao90@gmail.com>
2025-08-21 17:24:49 -0700

    Time: 199 ms (97.76%), Space: 131.9 MB (99.36%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
         int n = grid.size(), m = grid[0].size();
         int l = m, r = -1, u = n, d = -1;
         for (int i = 0; i < n; ++i) {
