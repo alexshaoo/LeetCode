ea3af58288edf08b7a00254a7b5426c518b95abc
Alex Shao <alexshao90@gmail.com>
2025-08-23 20:45:33 -0700

    Time: 0 ms (100%), Space: 60.1 MB (23.29%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        // dp[k][d] stores current length of 1s
-        // up to elemnt k, 0 means not deleted yet, 1 means deleted
         int n = nums.size();
-        int zeroCount = 0;
         int ans = 0;
+        int zeroCount = 0;
         int l = 0;
-        bool zero = false;
         for (int r = 0; r < n; ++r) {
-            if (nums[r] == 0) {
-                ++zeroCount;
-                zero = true;
-            }
+            zeroCount += (nums[r] == 0);
             while (zeroCount > 1) {
                 zeroCount -= (nums[l] == 0);
                 ++l;
             }
-            if (zeroCount == 1) {
-                ans = max(ans, r-l);
-            }
+            ans = max(ans, r-l);
         }
-        return zero ? ans : n-1;
+        return ans;
     }
 };
