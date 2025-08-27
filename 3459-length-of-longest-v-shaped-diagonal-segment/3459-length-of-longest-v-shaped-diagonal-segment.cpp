7302cd1c57d69fb4d5d0e5300c6568c3c59b6e6f
Alex Shao <alexshao90@gmail.com>
2025-08-26 21:47:03 -0700

    Time: 1645 ms (28.34%), Space: 95.4 MB (90.91%) - LeetHub

--- 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3459-length-of-longest-v-shaped-diagonal-segment/3459-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -3,13 +3,14 @@ public:
     vector<vector<int>> dirs = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}, {-1, -1}}; // indices 1-4 are dirs
     int backtrack(vector<vector<int>>& grid, int r, int c, int numToMatch, int dir, bool turned) {
         if (!(r >= 0 && r < grid.size() && c >= 0 && c < grid[0].size() && grid[r][c] == numToMatch)) {
-            return 1;
+            return 0;
         }
+        numToMatch = (numToMatch <= 1 ? 2 : 0);
         int right = 0;
         if (!turned) {
-            right = backtrack(grid, r + dirs[dir+1][0], c + dirs[dir+1][1], 2-numToMatch, dir+1, true);
+            right = backtrack(grid, r + dirs[dir+1][0], c + dirs[dir+1][1], numToMatch, dir+1, true);
         }
-        int straight = backtrack(grid, r + dirs[dir][0], c + dirs[dir][1], 2-numToMatch, dir, turned);
+        int straight = backtrack(grid, r + dirs[dir][0], c + dirs[dir][1], numToMatch, dir, turned);
         return 1 + max(straight, right);
     }
     int lenOfVDiagonal(vector<vector<int>>& grid) {
@@ -18,7 +19,7 @@ public:
             for (int j = 0; j < grid[0].size(); ++j) {
                 if (grid[i][j] == 1) {
                     for (int d = 0; d < 4; ++d)
-                        ans = max(ans, backtrack(grid, i+dirs[d][0], j+dirs[d][1], 2, d, false));
+                        ans = max(ans, backtrack(grid, i, j, 1, d, false));
                 }
             }
         }
