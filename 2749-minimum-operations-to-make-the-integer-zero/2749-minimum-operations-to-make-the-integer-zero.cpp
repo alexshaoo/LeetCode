1a3bf990622319cb400879cb8a53d2879e30028e
Alex Shao <alexshao90@gmail.com>
2025-09-04 22:40:04 -0400

    Time: 0 ms (100%), Space: 8.2 MB (85.95%) - LeetHub

---
+++ 2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numOnes(long long num) {
+        if (num == 0) return 0;
+        return (num&1) ? 1+numOnes(num/2) : numOnes(num/2);
+    }
+    int makeTheIntegerZero(int num1, int num2) {
+        long long n1 = (long long)num1, n2 = (long long)num2;
+        for (long long i = 1; i <= 60; ++i) {
+            long long res = n1 - i * num2;
+            if (i >= numOnes(res) && i <= res) return i;
+        }
+        return -1;
+    }
+};
