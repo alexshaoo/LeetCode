17a7bbec143fa0a1353fc30c901e44c5425b2896
Alex Shao <alexshao90@gmail.com>
2025-09-06 01:16:48 -0400

    Time: 110 ms (28.99%), Space: 207.3 MB (28.26%) - LeetHub

--- 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.cpp
+++ 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    long long f(long long num) {
-        long long d = 0;
-        while (num > 0) {
-            ++d;
-            num /= 4;
+    // number of operations needed for [1, num]
+    long long f(int num) {
+        long long res = 0, p = 4, val = 1, factor = 1;
+        while (4*val <= num) {
+            res += (p-val) * factor;
+            ++factor;
+            p *= 4;
+            val *= 4;
         }
-        return d;
+        res += (num-val+1)*factor;
+        return res;
+        
     }
     long long minOperations(vector<vector<int>>& queries) {
-        vector<long long> p;
-        for (long long i = 1; i <= 4*1e9+5; i *= 4) p.push_back(i);
-        long long sm = 0;
+        long long ans = 0;
         for (auto q : queries) {
-            long long l = q[0], r = q[1];
-            if (r-l == 1) {
-                sm += f(r);
-            } else {
-                long long lp = 0, rp = 0;
-                while (p[lp+1] <= l) ++lp;
-                while (p[rp+1] <= r) ++rp;
-                long long c = 0;
-                for (long long i = lp; i <= rp; ++i) {
-                    c += (p[i+1]-p[i])*(i+1);
-                }
-                // trim off left and right
-                c -= (l-p[lp])*(lp+1);
-                c -= (p[rp+1]-r-1)*(rp+1);
-                sm += (c+1)/2;
-            }
+            long long cnt = f(q[1]) - f(q[0]-1);
+            ans += (cnt + 1) / 2;
         }
-        return sm;
+        return ans;
     }
 };
