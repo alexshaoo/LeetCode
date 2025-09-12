05cc435c841efcc1e7f779d1c47e5303e1bd906f
Alex Shao <alexshao90@gmail.com>
2025-09-11 20:38:39 -0400

    Add 3462_vowels_game_in_a_string.cpp - LeetCode Problem 3462

---
+++ 3462_vowels_game_in_a_string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+  bool doesAliceWin(string s) {
+    string vowels = "aeiou";
+    vector<bool> isVowel(26, false);
+    for (char v : vowels) isVowel[v-'a'] = true;
+    return any_of(s.begin(), s.end(), [&](char c) { return isVowel[c-'a'];} );
+  }
+};
