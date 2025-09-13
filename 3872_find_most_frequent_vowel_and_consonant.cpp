8c1a1a9b7c78a595acb26871eabcec80109b297d
Alex Shao <alexshao90@gmail.com>
2025-09-13 06:00:43 -0400

    Update 3872_find_most_frequent_vowel_and_consonant.cpp - LeetCode Problem 3872

--- 3872_find_most_frequent_vowel_and_consonant.cpp
+++ 3872_find_most_frequent_vowel_and_consonant.cpp
@@ -4,17 +4,12 @@ public:
     string vowels = "aeiou";
     vector<bool> isVowel(26, false);
     for (char v : vowels) isVowel[v-'a'] = true;
-    map<char, int> v, c;
-    for (char ch : s) {
-      if (isVowel[ch-'a']) v[ch]++;
-      else c[ch]++;
-    }
+    unordered_map<char, int> mp;
     int vm = 0, cm = 0;
-    for (auto& [ch, cnt] : v) {
-      vm = max(vm, cnt);
-    }
-    for (auto& [ch, cnt] : c) {
-      cm = max(cm, cnt);
+    for (const char& ch : s) {
+      mp[ch]++;
+      if (isVowel[ch-'a']) vm = max(vm, mp[ch]);
+      else cm = max(cm, mp[ch]);
     }
     return vm+cm;
   }
