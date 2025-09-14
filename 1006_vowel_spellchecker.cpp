2305023fa8e4c544918e71e65d378b66c0960ae4
Alex Shao <alexshao90@gmail.com>
2025-09-14 00:56:17 -0400

    Update 1006_vowel_spellchecker.cpp - LeetCode Problem 1006

--- 1006_vowel_spellchecker.cpp
+++ 1006_vowel_spellchecker.cpp
@@ -1,63 +1,43 @@
 class Solution {
 public:
   string f(string a, int (&isVowel)[26]) {
-    string s = "";
-    for (const auto& ch : a) {
-      if (isVowel[ch-'a']) s += '1';
-      else s += ch;
+    a = l(a);
+    for (auto& ch : a) {
+      if (isVowel[ch-'a']) ch = '1';
     }
-    return s;
+    return a;
   }
-  string l(string a, int d) {
-    string s = "";
-    for (const auto& c : a) {
-      if (c >= 'A' && c <= 'Z') s += c+d;
-      else s += c;
-    }
-    return s;
+  string l(string a) {
+    for (auto& ch : a) ch = tolower(ch);
+    return a;
   }
   vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
     int n = wordlist.size();
     string vowels = "aeiou";
     int isVowel[26];
-    int d = 'a'-'A';
     for (const auto& v : vowels) isVowel[v-'a'] = true;
     vector<string> ans;
-    unordered_set<string> s;
-    for (const auto& w : wordlist) s.insert(w);
+    unordered_set<string> s(wordlist.begin(), wordlist.end());
     unordered_map<string, string> mplower, mpvowel;
-    for (int i = 0; i < n; ++i) {
-      string lower = l(wordlist[i], d);
-      if (mplower.count(lower) == 0) mplower[lower] = wordlist[i];
-      string converted = f(lower, isVowel);
-      if (mpvowel.count(converted) == 0) mpvowel[converted] = wordlist[i];
+    for (const auto& w : wordlist) {
+      string lower = l(w), converted = f(w, isVowel);
+      mplower.insert({lower, w});
+      mpvowel.insert({converted, w});
     }
-    unordered_map<string, string> mp;
-    for (const auto& q : queries) {
-      if (mp.contains(q)) {
-        ans.push_back(mp[q]);
+    for (int i = 0; i < queries.size(); ++i) {
+      string q = queries[i];
+      if (s.count(q)) continue;
+      string lower = l(q), converted = f(q, isVowel);
+      if (mplower.count(lower)) {
+        queries[i] = mplower[lower];
         continue;
       }
-      if (s.count(q)) {
-        ans.push_back(q);
-        mp[q] = q;
+      if (mpvowel.count(converted)) {
+        queries[i] = mpvowel[converted];
         continue;
       }
-      string qq = l(q, d);
-      if (mplower.count(qq)) {
-        ans.push_back(mplower[qq]);
-        mp[qq] = mplower[qq];
-        continue;
-      }
-      string qqq = f(qq, isVowel);
-      if (mpvowel.count(qqq)) {
-        ans.push_back(mpvowel[qqq]);
-        mp[qqq] = mpvowel[qqq];
-        continue;
-      }
-      ans.push_back("");
-      mp[q] = "";
+      queries[i] = "";
     }
-    return ans;
+    return queries;
   }
 };
