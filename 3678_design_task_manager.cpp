8190b8bf80aa64054cabee174b3d82145bbae380
Alex Shao <alexshao90@gmail.com>
2025-09-18 00:10:09 -0400

    Update 3678_design_task_manager.cpp - LeetCode Problem 3678

--- 3678_design_task_manager.cpp
+++ 3678_design_task_manager.cpp
@@ -1,45 +1,41 @@
 class TaskManager {
 public:
-    unordered_map<int, pair<int, int>> mp; // taskId -> {userId, priority}}
-    struct Cmp {
-        bool operator()(const vector<int>& a, const vector<int>& b) const {
-            if (a[2] != b[2]) return a[2] > b[2];
-            return a[1] > b[1];
-        }
-    };
-    set<vector<int>, Cmp> s;
+    unordered_map<int, pair<int, int>> mp; // taskId -> {userId, priority}
+    priority_queue<pair<int, int>> pq; // {priority, taskId}
     TaskManager(vector<vector<int>>& tasks) {
         for (const auto& task : tasks) {
             int u = task[0], t = task[1], p = task[2];
             mp[t] = {u, p};
-            s.insert(task);
+            pq.push({p, t});
         }
     }
     
     void add(int userId, int taskId, int priority) {
         mp[taskId] = {userId, priority};
-        vector<int> v = {userId, taskId, priority};
-        s.insert(v);
+        pq.push({priority, taskId});
     }
     
     void edit(int taskId, int newPriority) {
-        auto [userId, _] = mp[taskId];
-        rmv(taskId);
-        add(userId, taskId, newPriority);
+        auto& [userId, priority] = mp[taskId];
+        priority = newPriority;
+        pq.push({newPriority, taskId});
     }
     
     void rmv(int taskId) {
-        auto [userId, priority] = mp[taskId];
         mp.erase(taskId);
-        vector<int> v = {userId, taskId, priority};
-        s.erase(s.find(v));
     }
     
     int execTop() {
-        if (s.empty()) return -1;
-        auto task = *s.begin();
-        rmv(task[1]);
-        return task[0];
+        while (!pq.empty()) {
+            auto [p, t] = pq.top(); pq.pop();
+            auto it = mp.find(t);
+            if (it == mp.end()) continue;
+            if (it->second.second != p) continue;
+            int u = it->second.first;
+            mp.erase(it);
+            return u;
+        }
+        return -1;
     }
 };
 
