fd737b5cf34ef6067d4e362d80cb62a59f75bb9c
Alex Shao <alexshao90@gmail.com>
2025-09-18 20:44:43 -0400

    Update 3797_design_spreadsheet.cpp - LeetCode Problem 3797

--- 3797_design_spreadsheet.cpp
+++ 3797_design_spreadsheet.cpp
@@ -1,42 +1,28 @@
 class Spreadsheet {
-  vector<vector<int>> spreadsheet{26};
+  unordered_map<string, int> mp;
 public:
-  Spreadsheet(int rows) {
-    for (int i = 0; i < 26; ++i) {
-      vector<int> tmp(rows+1, 0);
-      spreadsheet[i] = tmp;
-    }
-  }
-  
+  Spreadsheet(int rows) {}
+
   void setCell(string cell, int value) {
-    int c = cell[0]-'A', r = stoi(cell.substr(1));
-    // cout <<c << ' ' << r << '\n';
-    spreadsheet[c][r] = value;
+    mp[cell] = value;
   }
   
   void resetCell(string cell) {
-    int c = cell[0]-'A', r = stoi(cell.substr(1));
-    spreadsheet[c][r] = 0;
+    mp[cell] = 0;
   }
   
+  int f(string s) {
+    if (s[0] >= 'A' && s[0] <= 'Z') {
+      if (mp.count(s)) return mp[s];
+      return 0;
+    }
+    return stoi(s);
+  }
+
   int getValue(string formula) {
-    int a = 0, b = 0;
-    int l = 1, sz = 0;
-    while (formula[l+sz] != '+') ++sz;
-    if (formula[l] >= 'A' && formula[l] <= 'Z') {
-      int c = formula[l]-'A', r = stoi(formula.substr(l+1, sz-1));
-      a = spreadsheet[c][r];
-    } else {
-      a = stoi(formula.substr(l, sz));
-    }
-    l += sz+1;
-    if (formula[l] >= 'A' && formula[l] <= 'Z') {
-      int c = formula[l]-'A', r = stoi(formula.substr(l+1));
-      b = spreadsheet[c][r];
-    } else {
-      b = stoi(formula.substr(l));
-    }
-    return a+b;
+    auto idx = formula.find('+');
+    string s = formula.substr(1, idx-1), t = formula.substr(idx+1);
+    return f(s)+f(t);
   }
 };
 
