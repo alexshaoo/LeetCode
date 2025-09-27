27186c62562c5eaec432639e23c126e005f898f3
Alex Shao <alexshao90@gmail.com>
2025-09-27 01:59:15 -0400

    Update 830_largest_triangle_area.cpp - LeetCode Problem 830

--- 830_largest_triangle_area.cpp
+++ 830_largest_triangle_area.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-  int det(vector<int>& a, vector<int>& b) {
-    return b[1] * a[0] - b[0] * a[1];
-  }
   double largestTriangleArea(vector<vector<int>>& points) {
     int n = points.size();
     double mx = 0.0;
-    for (int i = 0; i < n-2; ++i) {
-      for (int j = i+1; j < n-1; ++j) {
-        for (int k = j+1; k < n; ++k) {
-          mx = max(mx, 0.5 * abs(det(points[i], points[j]) + det(points[j], points[k]) + det(points[k], points[i])));
+    for (const auto& a : points) {
+      for (const auto& b : points) {
+        for (const auto& c : points) {
+          double val = 0.5*((b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0]));
+          mx = max(mx, max(val, -val)); 
         }
       }
     }
