7b7f3a39fe107a5363e14c13ec3f083e13860263
Alex Shao <alexshao90@gmail.com>
2025-10-02 00:42:43 -0400

    Update 3336_water_bottles_ii.cpp - LeetCode Problem 3336

--- 3336_water_bottles_ii.cpp
+++ 3336_water_bottles_ii.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
   int maxBottlesDrunk(int numBottles, int numExchange) {
-    int ans = 0;
-    int empty = 0, full = numBottles;
-    while (full > 0 || empty >= numExchange) {
-      // cout << full << ' ' << empty << ' ' << numExchange << '\n';
-      ans += full;
-      empty += full;
-      full = 0;
-      int cnt = 0, sm = 0;
-      while (sm + numExchange <= empty) {
-        ++cnt;
-        sm += numExchange;
-        ++numExchange;
-      }
-      full = cnt;
-      empty -= sm;
+    int cnt = numBottles;
+    for (;numExchange<=numBottles;) {
+      numBottles -= numExchange;
+      ++numBottles;
+      ++cnt;
+      ++numExchange;
     }
-    return ans;
+    return cnt;
   }
 };
