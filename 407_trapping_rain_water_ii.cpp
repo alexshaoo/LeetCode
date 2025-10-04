14df9f4bacbd89c16c5474551576d6f533b1e72b
Alex Shao <alexshao90@gmail.com>
2025-10-04 16:41:56 -0400

    Update 407_trapping_rain_water_ii.cpp - LeetCode Problem 407

--- 407_trapping_rain_water_ii.cpp
+++ 407_trapping_rain_water_ii.cpp
@@ -1,36 +1,43 @@
 class Solution {
 public:
   int trapRainWater(vector<vector<int>>& heightMap) {
-    int n = heightMap.size();
-    int m = heightMap[0].size();
-    int v = 0;
+    int n = heightMap.size(), m = heightMap[0].size();
     vector<vector<bool>> vis(n, vector<bool>(m, false));
-    auto cmp = [&](const array<int, 3>& a, const array<int, 3>& b) {
-      return a[0] > b[0];
+    // auto cmp = [&](const array<int, 3>& a, const array<int, 3>& b) {
+    //   return a[2] > b[2];
+    // };
+    struct cmp {
+      bool operator()(const array<int, 3>& a, const array<int, 3>& b) const {
+        return a[2] > b[2];
+      }
     };
-    priority_queue<array<int, 3>, vector<array<int, 3>>, decltype(cmp)> mh(cmp);
+    priority_queue<array<int, 3>, vector<array<int, 3>>, cmp> mh;
+    auto f = [&](int r, int c) {
+      if (!vis[r][c]) {
+        vis[r][c] = true;
+        mh.push({r, c, heightMap[r][c]});
+      }
+    };
     for (int i = 0; i < n; ++i) {
-      mh.push({heightMap[i][0], i, 0});
-      mh.push({heightMap[i][m-1], i, m-1});
-      vis[i][0] = true;
-      vis[i][m-1] = true;
+      f(i, 0);
+      f(i, m-1);
     }
     for (int i = 0; i < m; ++i) {
-      mh.push({heightMap[0][i], 0, i});
-      mh.push({heightMap[n-1][i], n-1, i});
-      vis[0][i] = true;
-      vis[n-1][i] = true;
+      f(0, i);
+      f(n-1, i);
     }
-    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    vector<vector<int>> dirs = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    vector<int> d = {-1, 0, 1, 0, -1};
+    int v = 0;
     while (!mh.empty()) {
-      auto [h, r, c] = mh.top(); mh.pop();
-      for (auto dir : dirs) {
-        int nr = r+dir[0], nc = c+dir[1];
-        if (nr >= 0 && nr < n && nc >= 0 && nc < m && !vis[nr][nc]) {
-          vis[nr][nc] = true;
-          if (heightMap[nr][nc] < h) v += (h-heightMap[nr][nc]);
-          mh.push({max(h, heightMap[nr][nc]), nr, nc});
-        }
+      auto [r, c, h] = mh.top(); mh.pop();
+      for (int i = 0; i < 4; ++i) {
+        int nr = r+d[i], nc = c+d[i+1];
+        if (!(nr >= 0 && nr < n && nc >= 0 && nc < m)) continue;
+        if (vis[nr][nc]) continue;
+        vis[nr][nc] = true;
+        v += max(0, h - heightMap[nr][nc]);
+        mh.push({nr, nc, max(h, heightMap[nr][nc])});
       }
     }
     return v;
