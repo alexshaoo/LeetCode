20ff0836c068c9268dd959baaa86cfb293935656
Alex Shao <alexshao90@gmail.com>
2025-10-09 23:14:17 -0400

    Update 3383_taking_maximum_energy_from_the_mystic_dungeon.cpp - LeetCode Problem 3383

--- 3383_taking_maximum_energy_from_the_mystic_dungeon.cpp
+++ 3383_taking_maximum_energy_from_the_mystic_dungeon.cpp
@@ -3,15 +3,11 @@ public:
   int maximumEnergy(vector<int>& energy, int k) {
     int n = energy.size();
     int mx = -1005;
-    vector<int> ps(n+k);
-    for (int i = 0; i < n; ++i) {
-      ps[i+k] = ps[i] + energy[i];
-    }
-    for (int i = n; i < n+k; ++i) {
-      int j = i%k;
-      while (j < n) {
-        mx = max(mx, ps[i]-ps[j]);
-        j += k;
+    for (int i = n-k; i < n; ++i) {
+      int sm = 0;
+      for (int j = 0; i-j >= 0; j += k) {
+        sm += energy[i-j];
+        mx = max(mx, sm);
       }
     }
     return mx;
