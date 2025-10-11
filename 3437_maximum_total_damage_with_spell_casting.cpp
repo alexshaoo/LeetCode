7eb79b3b8bdc277d4cdce6d04a6f48ccbb67a1b2
Alex Shao <alexshao90@gmail.com>
2025-10-11 02:07:50 -0400

    Update 3437_maximum_total_damage_with_spell_casting.cpp - LeetCode Problem 3437

--- 3437_maximum_total_damage_with_spell_casting.cpp
+++ 3437_maximum_total_damage_with_spell_casting.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
   long long maximumTotalDamage(vector<int>& power) {
-    set<int> s;
-    for (int x : power) s.insert(x);
-    int n = s.size();
-    vector<int> v(s.begin(), s.end());
-    unordered_map<int, int> mp;
-    for (int x : power) mp[x]++;
-    vector<vector<long long>> dp(n, vector<long long>(2, 0));
-    for (int i = 0; i < n; ++i) {
-      // did not cast current
-      if (i > 0) {
-        dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
-      }
-      // cast current
-      dp[i][1] = mp[v[i]]*(long long)v[i];
-      int idx = i;
-      while (idx >= 0 && v[idx]+2 >= v[i]) --idx;
-      if (idx >= 0) {
-        dp[i][1] += max(dp[idx][0], dp[idx][1]);
+    int n = power.size();
+    sort(power.begin(), power.end());
+    vector<long long> dp(n+1, 0);
+    long long mx = 0;
+    for (int i = 0, j = 0; i < n; ++i) {
+      if (i == 0 || power[i] == power[i-1]) {
+        dp[i+1] = dp[i] + power[i];
+      } else {
+        while (power[j] < power[i]-2) {
+          mx = max(mx, dp[j+1]);
+          ++j;
+        }
+        dp[i+1] = power[i] + mx;
       }
     }
-    // for (int i = 0; i < n; ++i) {
-    //   cout << dp[i][0] << ' ' << dp[i][1] << '\n';
-    // }
-    // cout << '\n';
-    return max(dp[n-1][0], dp[n-1][1]);
+    return *max_element(dp.begin(), dp.end());
   }
 };
