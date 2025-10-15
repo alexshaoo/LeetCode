f62093e68757d28d68619c4d09476725e02baf10
Alex Shao <alexshao90@gmail.com>
2025-10-14 23:33:59 -0400

    Update 3619_adjacent_increasing_subarrays_detection_ii.cpp - LeetCode Problem 3619

--- 3619_adjacent_increasing_subarrays_detection_ii.cpp
+++ 3619_adjacent_increasing_subarrays_detection_ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
   int maxIncreasingSubarrays(vector<int>& nums) {
-    const int MIN = -(1e9+5);
-    nums.push_back(MIN);
-    int last = MIN, currLen = 0, prevLen = 0;
+    int n = nums.size();
+    int currLen = 1, prevLen = 0;
     int ans = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-      if (nums[i] <= last) {
-        ans = max(ans, min(prevLen, currLen));
-        ans = max(ans, currLen/2);
+    for (int i = 1; i < n; ++i) {
+      if (nums[i] <= nums[i-1]) {
         prevLen = currLen;
         currLen = 1;
       } else {
         ++currLen;
       }
-      last = nums[i];
+      ans = max(ans, min(prevLen, currLen));
+      ans = max(ans, currLen/2);
     }
     return ans;
   }
