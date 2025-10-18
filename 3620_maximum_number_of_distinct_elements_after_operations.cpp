629229e09deae8ffdc4aa81f5967d4040209ed7c
Alex Shao <alexshao90@gmail.com>
2025-10-18 02:56:34 -0400

    Update 3620_maximum_number_of_distinct_elements_after_operations.cpp - LeetCode Problem 3620

--- 3620_maximum_number_of_distinct_elements_after_operations.cpp
+++ 3620_maximum_number_of_distinct_elements_after_operations.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
   int maxDistinctElements(vector<int>& nums, int k) {
-    int n = nums.size();
     sort(nums.begin(), nums.end());
-    int distinct = 1;
-    long long transformed = nums[0]-k;
-    for (int i = 1; i < n; ++i) {
-      long long l = nums[i]-k, r = nums[i]+k;
-      if (transformed+1 < l) {
+    int distinct = 0;
+    int prev = INT_MIN;
+    for (int num : nums) {
+      if (prev < num+k) {
         ++distinct;
-        transformed = l;
-      } else if (transformed+1 >= l && transformed+1 <= r) {
-        ++distinct;
-        transformed++;
+        prev = max(prev+1, num-k);
       }
     }
     return distinct;
