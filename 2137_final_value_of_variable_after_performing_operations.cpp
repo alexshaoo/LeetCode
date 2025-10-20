5fb1af0f6398e2c136a0a47ce967f6f21caabc7e
Alex Shao <alexshao90@gmail.com>
2025-10-20 00:21:03 -0400

    Update 2137_final_value_of_variable_after_performing_operations.cpp - LeetCode Problem 2137

--- 2137_final_value_of_variable_after_performing_operations.cpp
+++ 2137_final_value_of_variable_after_performing_operations.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
   int finalValueAfterOperations(vector<string>& operations) {
-    unordered_map<string, int> mp;
-    mp["--X"] = -1;
-    mp["X--"] = -1;
-    mp["++X"] = 1;
-    mp["X++"] = 1;
     int x = 0;
-    for (auto op : operations) x += mp[op];
+    for (auto op : operations) x += (op[1] == '+' ? 1 : -1);
     return x;
   }
 };
