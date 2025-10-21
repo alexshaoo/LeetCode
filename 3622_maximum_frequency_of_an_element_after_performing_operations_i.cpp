0d9c08809edc2890c7245979bb9e2020ce9f33dc
Alex Shao <alexshao90@gmail.com>
2025-10-20 20:45:43 -0400

    Update 3622_maximum_frequency_of_an_element_after_performing_operations_i.cpp - LeetCode Problem 3622

--- 3622_maximum_frequency_of_an_element_after_performing_operations_i.cpp
+++ 3622_maximum_frequency_of_an_element_after_performing_operations_i.cpp
@@ -8,10 +8,9 @@ public:
     int l = 0, r = 0;
     int ans = 0;
     for (int num = nums[0]-k; num <= nums[n-1]+k; ++num) {
-      while (r < n-1 && num+k >= nums[r+1]) ++r;
-      while (l <= r && num-k > nums[l]) ++l;
-      if (r-l+1-freq[num] > numOperations) ans = max(ans, freq[num]+numOperations);
-      else ans = max(ans, r-l+1);
+      while (r < n && num+k >= nums[r]) ++r;
+      while (l < r && num-k > nums[l]) ++l;
+      ans = max(ans, min(r-l, freq[num]+numOperations));
     }
     return ans;
   }
