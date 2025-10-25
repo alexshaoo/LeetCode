2d35466566755929d7209b34f66851ce4c875d31
Alex Shao <alexshao90@gmail.com>
2025-10-25 16:37:16 -0400

    Update 1817_calculate_money_in_leetcode_bank.cpp - LeetCode Problem 1817

--- 1817_calculate_money_in_leetcode_bank.cpp
+++ 1817_calculate_money_in_leetcode_bank.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
   int totalMoney(int n) {
-    int mon = 0;
-    int amt = 0;
-    int ans = 0;
-    for (int i = 0; i < n; ++i) {
-      if (i%7 == 0) {
-        ++mon;
-        amt = mon;
-      } else {
-        ++amt;
-      }
-      ans += amt;
-    }
-    return ans;
+    int w = n/7;
+    int d = n%7;
+    int ww = 7 * ((w + 3) * (w + 4) / 2 - 6);
+    int dd = (w+d)*(w+d+1)/2 - w*(w+1)/2;
+    return ww + dd;
   }
 };
