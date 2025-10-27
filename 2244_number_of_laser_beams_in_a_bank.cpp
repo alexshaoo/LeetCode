6d42bbffee806002735fc8e9c70e8d4b7eb89bb5
Alex Shao <alexshao90@gmail.com>
2025-10-27 18:07:18 -0400

    Update 2244_number_of_laser_beams_in_a_bank.cpp - LeetCode Problem 2244

--- 2244_number_of_laser_beams_in_a_bank.cpp
+++ 2244_number_of_laser_beams_in_a_bank.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
   int numberOfBeams(vector<string>& bank) {
-    vector<int> layers;
-    for (string row : bank) {
-      int cnt = 0;
-      for (char c : row) if (c == '1') ++cnt;
-      if (cnt > 0) layers.push_back(cnt);
-    }
-    if (layers.size() < 2) return 0;
     int ans = 0;
-    for (int i = 0; i < layers.size()-1; ++i) ans += layers[i] * layers[i+1];
+    int prev = 0;
+    for (int i = 0; i < bank.size(); ++i) {
+      int cnt = 0;
+      for (char c : bank[i]) if (c == '1') ++cnt;
+      if (cnt > 0) {
+        ans += cnt * prev;
+        prev = cnt;
+      }
+    }
     return ans;
   }
 };
