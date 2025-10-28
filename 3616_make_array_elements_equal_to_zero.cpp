24b3e255cfda40fd13daa397e09e480a2c3aead4
Alex Shao <alexshao90@gmail.com>
2025-10-27 23:57:02 -0400

    Update 3616_make_array_elements_equal_to_zero.cpp - LeetCode Problem 3616

--- 3616_make_array_elements_equal_to_zero.cpp
+++ 3616_make_array_elements_equal_to_zero.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-  bool sim(vector<int> nums, int pos, bool left) {
-    while (pos >= 0 && pos < nums.size()) {
-      if (nums[pos] != 0) { 
-        nums[pos]--;
-        left = !left;
-      }
-      pos += (left ? -1 : 1);
-    }
-    for (int num : nums) if (num != 0) return false;
-    return true;
-  }
   int countValidSelections(vector<int>& nums) {
     int cnt = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-      if (nums[i] != 0) continue;
-      cnt += sim(nums, i, true) + sim(nums, i, false);
+    int sm = accumulate(nums.begin(), nums.end(), 0);
+    int lsm = 0;
+    int idx = 0;
+    auto f = [&](int inc) {
+      while (idx < nums.size() && nums[idx] == 0) {
+        cnt += inc;
+        ++idx;
+      }
+    };
+    while (idx < nums.size()) {
+      if (nums[idx] != 0) {
+        lsm += nums[idx];
+        ++idx;
+      } else {
+        if (sm % 2 == 0) {
+          if (lsm == sm/2) {
+            f(2);
+            break;
+          } else f(0);
+        } else {
+          if (lsm == sm/2) {
+            f(1);
+          } else if (lsm == sm/2+1) {
+            f(1);
+            break;
+          } else f(0);
+        }
+      }
     }
     return cnt;
   }
