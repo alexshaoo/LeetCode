4e07957875f496297283f1f89dbac9a7328501dc
Alex Shao <alexshao90@gmail.com>
2025-11-01 20:51:10 -0400

    Add 2343_count_unguarded_cells_in_the_grid.cpp - LeetCode Problem 2343

---
+++ 2343_count_unguarded_cells_in_the_grid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+  int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+    vector<int> dirs = {0, -1, 0, 1, 0};
+    vector<vector<int>> seen(m, vector<int>(n, 0)); // can be seen by guard is 1, guard/wall is 2
+    for (const auto& wall : walls) {
+      seen[wall[0]][wall[1]] = 2;
+    }
+    for (const auto& guard : guards) {
+      seen[guard[0]][guard[1]] = 2;
+    }
+    int occ = walls.size() + guards.size();
+    for (const auto& guard : guards) {
+      int r = guard[0], c = guard[1];
+      for (int i = 0; i < 4; ++i) {
+        int nr = r, nc = c;
+        while (true) {
+          nr += dirs[i];
+          nc += dirs[i+1];
+          if (min(nr, nc) < 0 || nr >= m || nc >= n || seen[nr][nc] == 2) break;
+          if (seen[nr][nc] != 1) {
+            ++occ;
+            seen[nr][nc] = 1;
+          }
+        }
+      }
+    }
+    return m*n-occ;
+  }
+};
