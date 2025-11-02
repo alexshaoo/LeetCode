0fea25ceadb172018facf6660108073dfe467c15
Alex Shao <alexshao90@gmail.com>
2025-11-01 20:54:54 -0400

    Update 2343_count_unguarded_cells_in_the_grid.cpp - LeetCode Problem 2343

--- 2343_count_unguarded_cells_in_the_grid.cpp
+++ 2343_count_unguarded_cells_in_the_grid.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
   int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
     vector<int> dirs = {0, -1, 0, 1, 0};
-    vector<vector<int>> seen(m, vector<int>(n, 0)); // can be seen by guard is 1, guard/wall is 2
+    vector<int> seen(m*n); // can be seen by guard is 1, guard/wall is 2
     for (const auto& wall : walls) {
-      seen[wall[0]][wall[1]] = 2;
+      seen[wall[0]*n+wall[1]] = 2;
     }
     for (const auto& guard : guards) {
-      seen[guard[0]][guard[1]] = 2;
+      seen[guard[0]*n+guard[1]] = 2;
     }
     int occ = walls.size() + guards.size();
     for (const auto& guard : guards) {
@@ -17,10 +17,10 @@ public:
         while (true) {
           nr += dirs[i];
           nc += dirs[i+1];
-          if (min(nr, nc) < 0 || nr >= m || nc >= n || seen[nr][nc] == 2) break;
-          if (seen[nr][nc] != 1) {
+          if (min(nr, nc) < 0 || nr >= m || nc >= n || seen[nr*n+nc] == 2) break;
+          if (seen[nr*n+nc] != 1) {
             ++occ;
-            seen[nr][nc] = 1;
+            seen[nr*n+nc] = 1;
           }
         }
       }
